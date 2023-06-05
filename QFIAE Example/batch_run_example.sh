05f19fa357258dcfd1bf3195b51d6bf7f2828286
Jorge J. Martínez de Lejarza <61199780+gmlejarza@users.noreply.github.com>
2023-06-05 10:30:43 +0200

    Update batch_run_example.sh

--- QFIAE Example/batch_run_example.sh
+++ QFIAE Example/batch_run_example.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#m the mass (a parameter that should go from 5 to 175 changing from 5 to 5 (5,175,5)
-#j a scaling parameter(0, or 2) when j=1 g(z)=0
 
 python main.py -in myconfig.json 
 
